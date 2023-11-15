818540a78755e07c7b4adb5d887ca6bdf76d2e3e
Hanibunia <tanhanyi21@gmail.com>
2023-11-15 23:38:25 +0800

    Added review unit tests and added get reviews and modified Register unit tests

--- tests/userTests.test.js
+++ tests/userTests.test.js
@@ -5,80 +5,133 @@ const sinon = require('sinon'); // Import sinon
 const bcrypt = require('bcrypt'); // Import bcrypt
 const { register, login } = require('../utils/UserUtil');
 
+const usersFilePath = 'utils/users.json';
+let orgContent = [];
+
 describe('Testing Register Function', () => {
     const usersFilePath = 'utils/users.json';
-    var orgContent = "";
+    let orgContent = [];
 
     beforeEach(async () => {
-        // Create a sinon stub for bcrypt.hash
-        const hashStub = sinon.stub(bcrypt, 'hash');
-        // Specify what the stub should return
-        hashStub.returns('mockedHashedPassword');
-
-        orgContent = await fs.readFile(usersFilePath, 'utf8');
-        orgContent = JSON.parse(orgContent);
+        try {
+            orgContent = await fs.readFile(usersFilePath, 'utf8');
+            orgContent = JSON.parse(orgContent);
+            console.log('Before test 1st:', orgContent);
+        } catch (error) {
+            console.error('Error reading or parsing users file:', error);
+            // You might want to throw the error or handle it appropriately based on your needs
+        }
     });
+    
 
-    afterEach(() => {
-        // Restore the original bcrypt.hash function
-        bcrypt.hash.restore();
+    afterEach(async () => {
+        console.log('After test  1st:', orgContent);
+        await fs.writeFile(usersFilePath, JSON.stringify(orgContent), 'utf8');
     });
 
     it('Should register a new user successfully', async () => {
-        const req = {
-            body: {
-                email: 'mary@gmail.com',
-                password: '123456',
-            },
-        };
-        const res = {
-            status: function (code) {
-                expect(code).to.equal(201);
-                return this;
-            },
-            json: function (data) {
-                expect(data).to.have.lengthOf(orgContent.length + 1);
-                expect(data[orgContent.length].email).to.equal(req.body.email);
-                expect(data[orgContent.length].password).to.equal('mockedHashedPassword'); // Check the hashed password
-            },
-        };
-        await register(req, res);
-    });
-    it('Should shows validation error due to email', async () => {
-        const req = {
-            body: {
-                email: 'simon#gmail.com',
-                password: '123',
-            },
-        };
-        const res = {
-            status: function (code) {
-                expect(code).to.equal(500);
-                return this;
-            },
-            json: function (data) {
-                expect(data.message).to.equal('Validation error');
-            },
-        };
-        await register(req, res);
-    });
+const testUserEmail = `unique_email_${Date.now()}@example.com`;
 
-    it('Should shows validation error due to password length', async () => {
-        const req = {
-            body: {
-                email: 'simon@gmail.com',
-                password: '123',
-            },
-        };
-        const res = {
-            status: function (code) {
-                expect(code).to.equal(500);
-                return this;
-            },
-            json: function (data) {
-                expect(data.message).to.equal('Validation error');
-            },
-        };
+    const req = {
+        body: {
+            email: testUserEmail,
+            password: '123456',
+        },
+    };
+    const res = {
+        status: function (code) {
+            console.log('Response status code:', code);
+            expect(code).to.equal(201);
+            return this;
+        },
+        json: function (data) {
+            console.log('Response data:', data); // Log the entire response data for debugging
+
+            // Add a check for data existence before performing assertions
+            if (data) {
+                // Expectations for the response data
+                expect(data).to.have.lengthOf(orgContent.length + 1);
+                const newUser = data[orgContent.length];
+                expect(newUser.email).to.equal(req.body.email);
+                // Adjust this expectation to check if the password is not the original password
+                expect(newUser.password).to.not.equal('123456');
+            } else {
+                // Log a warning if data is undefined
+                console.warn('Response data is undefined.');
+            }
+        },
+    };
+
+    await register(req, res);
+});
+});
+it('Should shows validation error due to email', async () => {
+    const req = {
+        body: {
+            email: 'simon#gmail.com',
+            password: '123',
+        },
+    };
+    const res = {
+        status: function (code) {
+            expect(code).to.equal(500);
+            return this;
+        },
+        json: function (data) {
+            expect(data.message).to.equal('Validation error');
+        },
+    };
+    await register(req, res);
+});
+
+it('Should shows validation error due to password length', async () => {
+    const req = {
+        body: {
+            email: 'simon@gmail.com',
+            password: '123',
+        },
+    };
+    const res = {
+        status: function (code) {
+            expect(code).to.equal(500);
+            return this;
+        },
+        json: function (data) {
+            expect(data.message).to.equal('Validation error');
+        },
+    };
+    await register(req, res);
+});
+it('Should not register a user if email already exists', async () => {
+    const existingEmail = 'unique_email_1700060061400@example.com';
+
+    const req = {
+        body: {
+            email: existingEmail,
+            password: 'newpassword123',
+        },
+    };
+    const res = {
+        status: function (code) {
+            expect(code).to.equal(400);  
+            return this;
+        },
+        json: function (data) {
+            expect(data.message).to.equal('Email already exists');
+            console.log('Response data message:', data.message);
+
+        },
+    };
+
+    try {
         await register(req, res);
-    });
+    } catch (error) {
+        console.error('Error during test execution:', error);
+        throw error;
+    }
+
+    // Update orgContent with the modified data after the test
+    orgContent = await fs.readFile(usersFilePath, 'utf8');
+    orgContent = JSON.parse(orgContent);
+    console.log('After test - orgContent:', orgContent);
 });
--- utils/ReviewUtil.js
+++ utils/ReviewUtil.js
@@ -5,7 +5,7 @@ async function readJSON(filename) {
     try {
         const data = await fs.readFile(filename, 'utf8');
         const allUsers = JSON.parse(data);
-        console.log("Loaded users from file:", allUsers); // Add this line for debugging
+        //console.log("Loaded users from file:", allUsers); // Add this line for debugging
         return allUsers;
     } catch (err) {
         console.error(err);
@@ -22,8 +22,47 @@ async function writeJSON(object, filename) {
     } catch (err) { console.error(err); throw err; }
 }
 
+async function viewReview(req, res) {
+    try {
+        const userEmail = req.params.userEmail;
+
+        if (!userEmail) {
+            return res.status(400).json({ message: 'Invalid data: Missing user email' });
+        }
+
+        const allReviews = await readJSON('utils/reviews.json');
+        const userReviews = allReviews.filter(entry => entry.userEmail === userEmail);
+
+        if (userReviews.length > 0) {
+            res.status(200).json(userReviews);
+        } else {
+            res.status(404).json({ message: 'No reviews found for the user' });
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 
 
+}
+
+
+async function viewAllReviews(req, res) {
+    try {
+        // Read all reviews from the file
+        const allReviews = await readJSON('utils/reviews.json');
+
+        // Check if there are any reviews
+        if (allReviews.length > 0) {
+            res.status(200).json(allReviews);
+        } else {
+            res.status(404).json({ message: 'No reviews found' });
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
 async function addReview(req, res) {
     try {
         const userEmail = req.body.userEmail;
@@ -42,23 +81,22 @@ async function addReview(req, res) {
             const allReviews = await readJSON('utils/reviews.json');
             allReviews.push(review);
 
-            await fs.writeFile('utils/reviews.json', JSON.stringify(allReviews), 'utf8');
+                await fs.writeFile('utils/reviews.json', JSON.stringify(allReviews), 'utf8');
 
-            res.status(201).json(allReviews.filter(entry => entry.userEmail === userEmail));
-        } else {
-            res.status(404).json({ message: 'User not found' });
+                res.status(201).json(allReviews.filter(entry => entry.userEmail === userEmail));
+            } else {
+                res.status(404).json({ message: 'User not found' });
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+            res.status(500).json({ message: 'Internal server error' });
         }
-    } catch (error) {
-        console.error('An error occurred:', error);
-        res.status(500).json({ message: 'Internal server error' });
     }
-}
 
-console.log("test")
 
 
 
 
 module.exports = {
-    readJSON, writeJSON, addReview
+    readJSON, writeJSON, addReview ,viewReview,viewAllReviews
 };
