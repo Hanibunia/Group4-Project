86fcb654749f95cd6251026376443d05626b281e
Clement Chng <clementchng12@gmail.com>
2024-02-01 20:32:30 +0800

    terraform

--- rms-terraform/main.tf
+++ rms-terraform/main.tf
@@ -1,29 +1,38 @@
 terraform {
-required_providers {
-azurerm = {
-source = "hashicorp/azurerm"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+        }
+    }
 }
-}
-}
+
+
+
+
+
 provider "azurerm" {
-features {}
+    features {}
+
 }
 resource "azurerm_resource_group" "dvopsResourceGroup" {
-name = "dvopsResourceGroup"
-location = "East US"
+    name = "dvopsResourceGroup"
+    location = "East US"
 }
 resource "azurerm_kubernetes_cluster" "dvopsAKSCluster" {
-name = "dvopsAKSCluster"
-location = azurerm_resource_group.dvopsResourceGroup.location
-resource_group_name = azurerm_resource_group.dvopsResourceGroup.name
-dns_prefix = "rms-aks"
-default_node_pool {
-name = "default"
-node_count = 1
-vm_size = "Standard_DS2_v2"
+  name                = "dvopsAKSCluster"
+  location            = azurerm_resource_group.dvopsResourceGroup.location
+  resource_group_name = azurerm_resource_group.dvopsResourceGroup.name
+  dns_prefix          = "rms-aks"
+
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "Standard_DS2_v2"
+  
 }
 service_principal {
-client_id = "97694f38-db4d-49ea-8a7c-2c8b33f88cf5"
-client_secret = "dNa8Q~4H49kNZZ~Gh_t2KYLUU9q1jsup8jeWMcir"
+    client_id = "e68da889-791b-44e8-8771-d0a335ed3218"
+    client_secret = "yEu8Q~IWYtUzMyFrClZOp~kjqmyGAVAMYrDPyap2"
+
 }
 }
